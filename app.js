0ff2cbbc372dcd3f89066fc6d320931e2b99f1db
samadon1 <calvinsamueldonkor@gmail.com>
2022-08-21 11:45:30 +0000

    particles edit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ particlesJS('particles-js',
         "value": 180,
         "density": {
           "enable": true,
-          "value_area": 5800
+          "value_area": 800
         }
       },
       "color": {
